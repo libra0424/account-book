6e11f2424d29ae1acea4c805fc32c4566e4e8326
ryotana <officework.tanaka@gmail.com>
2022-04-19 09:51:33 +0900

    netlify用に変更

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: isProd ? '/account-book' : '/',
-  outputDir: 'docs',
-  filenameHashing: false,
-  productionSourceMap: false
 }
