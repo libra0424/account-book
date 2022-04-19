0afe3a6ead19f4f00646c696d0628961d3ce7b78
ryotana <officework.tanaka@gmail.com>
2022-04-19 09:31:10 +0900

    GitHub公開用の設定に変更

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publishPath: isProd ? '/account-book' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
